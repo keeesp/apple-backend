b564f7fd4a4c4e39ea5293d4a9b4b933833ad694
kirill shubrat <kirill.shubrat@mail.ru>
2023-05-12 13:09:47 +0300

    initial commit

---
+++ src/statistics/statistics.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { UserService } from 'src/user/user.service'
+
+@Injectable()
+export class StatisticsService {
+	constructor(
+		private prisma: PrismaService,
+		private userService: UserService
+	) {}
+
+	async getMain(userId: number) {
+		const user = await this.userService.byId(userId, {
+			orders: {
+				select: {
+					items: {
+						select: {
+							price: true
+						}
+					}
+				}
+			},
+			reviews: true
+		})
+
+		/* PLEASE DON'T USE IN PRODUCTION */
+
+		/* for (let order of user.orders) {
+			let total = 0
+			for (let vote of order.) {
+				vote_sum += vote.value
+			}
+			;(question as any).vote_sum = vote_sum
+		} */
+
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},
+			{
+				name: 'Reviews',
+				value: user.reviews.length
+			},
+			{
+				name: 'Favorites',
+				value: user.favorites.length
+			},
+			{
+				name: 'Total amount',
+				value: 1000
+			}
+		]
+	}
+}
